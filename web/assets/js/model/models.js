3427b10142a6201df5a6051f3b44292c5faf698d
محمد علی پور فتحکوهی <alipour.m@live.com>
2023-06-09 20:39:04 +0330

    Update models.js

--- web/assets/js/model/models.js
+++ web/assets/js/model/models.js
@@ -190,7 +190,7 @@ class AllSetting {
         this.subPort = "2096";
         this.subPath = "/sub/";
         this.subDomain = "";
-		this.CloudDomain = "";
+		this.cloudDomain = "";
         this.subCertFile = "";
         this.subKeyFile = "";
         this.subUpdates = 0;
